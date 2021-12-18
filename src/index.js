b83e762cddce851d0b070b67329cb9c24e46a801
saidinesh898 <saidinesh898@gmail.com>
2021-12-18 20:24:13 +0530

    commit before auth

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
+
 const port = process.env.PORT || 80
 
 app.use(express.json())
@@ -12,9 +13,22 @@ app.use(taskRouter)
 
 
 app.get("/", (req, res) => {
-    res.status(401).send("No Content Here Use API")
+    res.status(400).send("No Content Here Use API")
+
 })
 
+const jwt = require('jsonwebtoken')
+const myFunction = async() => {
+    100
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismysecret', { expiresIn: '1 year' })
+    try {
+        console.log(jwt.verify(token, 'thisismysecret'))
+    } catch (e) {
+        console.log("invalid token")
+    }
+
+}
+myFunction()
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
--- src/routers/user.js
+++ src/routers/user.js
@@ -19,6 +19,16 @@ router.post('/users', async(req, res) => {
     }
 })
 
+router.post('/users/login', async(req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (e) {
+        res.status(400).send()
+    }
+
+})
+
 //* READ All Users
 
 router.get('/users', async(req, res) => {
@@ -55,7 +65,9 @@ router.patch('/users/:id', async(req, res) => {
         return res.status(400).send({ error: 'Invalid updates!' })
     }
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
         if (!user) {
             return res.status(404).send('User not found')
         }
