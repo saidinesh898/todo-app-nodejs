5ffe195fb49eef6519015d024d20f3ae58ea2d32
saidinesh898 <saidinesh898@gmail.com>
2021-12-21 13:43:56 +0530

    updates task route

--- src/routers/task.js
+++ src/routers/task.js
@@ -23,27 +23,21 @@ router.post('/tasks', auth, async(req, res) => {
 
 router.get('/tasks/', auth, async(req, res) => {
     const match = {}
-    const sort = {
-        createdAt: 1
+    const sort = {}
+
+    if (req.query.statuis) {
+        match.completed = req.query.completed === 'true'
     }
-    if (req.query.status) {
-        match.status = req.query.status === 'true'
-    }
+
     if (req.query.sortBy) {
         const parts = req.query.sortBy.split(':')
         sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
     }
+
+
     try {
-        await req.user.populate({
-            path: 'tasks',
-            match,
-            options: {
-                limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip),
-                sort
-            }
-        })
-        res.send(req.user.tasks)
+        const task = await Task.find({ owner: req.user._id })
+        res.send(task)
     } catch (e) {
         res.status(500).send(e)
     }
