6f2a6b38069178ff47cd723d6a8d7039c9bd8494
Sai_Dinesh <saidinesh@texsync.com>
2022-01-07 04:23:16 +0530

    ..

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,10 +1,8 @@
 require("dotenv").config();
 
-MONGODB_URI ="mongodb+srv://saidinesh898:9962341414@aws-mum-cluster.hvt8w.mongodb.net/task-manager-api";
-
 const mongoose = require("mongoose");
 try {
-  mongoose.connect(MONGODB_URI, {
+  mongoose.connect(process.env.MONGODB_REMOTE, {
     useNewUrlParser: true,
   });
 } catch (e) {
