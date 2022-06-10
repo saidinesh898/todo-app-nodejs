a5d59f08b436c213a130a893f0828228e0c4d6e2
Sai Dinesh <60782915+saidinesh898@users.noreply.github.com>
2022-06-10 20:40:19 +0530

    Update index.js
    Removed unwanted routes

--- src/index.js
+++ src/index.js
@@ -17,17 +17,6 @@ app.use(taskRouter)
 
 
 
-app.get("/tcs", (req, res) => {
-    res.send("<h1>Hello TCS</h1>")
-})
-
-app.get("*", (req, res) => {
-    res.status(404).send(
-        `<h1>404</h1>
-        <h2>Page not found Modified</h2>`
-    )
-})
-
 
 
 app.listen(port, () => {
