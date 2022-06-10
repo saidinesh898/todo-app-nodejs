de1b044714cae4ad4648e6152dfbe0f3e0dd09ef
Sai Dinesh <saidinesh898@gmail.com>
2022-06-10 21:06:28 +0530

    fixed codebase

--- testfaker.js
+++ testfaker.js
@@ -3,4 +3,4 @@ var faker = require('faker');
 
 var randomName = faker.image.image(); // Rowan Nikolaus
 var randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
-console.log(randomName)
+console.log()
